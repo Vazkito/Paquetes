a4c722ef02af807a79e230216b20333c07b7299c
David <davidevaz85@gmail.com>
2015-10-06 16:17:01 +0200

    Programa_ok

--- Detectar_letras.py
+++ Detectar_letras.py
@@ -11,7 +11,7 @@ for indice,elementos in enumerate(palabra):#enumerate para recorrer la lista cua
     if indice != len(palabra)  : # len (lista) es la longitud total de la lista sin expecificar cuanto ocupe 
         if palabra.count(elementos) > 1: #Recorremos de principio a fin la lista con un bucle for y en cada parada hacemos una comprobación: si el número de veces que aparece ese elemento es mayor que uno, lo mostramos.
             resultados.append(elementos)
-            print(resultados)
+print(resultados)
         
 
 
